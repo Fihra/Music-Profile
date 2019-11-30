c67821f73c7f1b75d0cc615c14f9656f517af93b
Fabian Fabro <fabrofabian@gmail.com>
2019-11-30 15:21:49 -0800

    setup home and signup component pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+import Home from './components/Home';
+import Signup from './components/Signup';
 
 class App extends React.Component {
   render(){
     return (
-      <div>
-        <h1>Music Profile</h1>
-      </div>
+      <Router>
+        {/* <div>
+        
+        </div> */}
+        <Route path="/" component={Home}/>
+        <Route path="/signup" exact component={Signup}/>
+      </Router>
+
     );
   }
 
