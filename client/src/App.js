7c94d1a9164eccd016c6a6c8628790fc8589c293
Fabian Fabro <fabrofabian@gmail.com>
2019-11-30 16:04:09 -0800

    navbar links to different pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 class App extends React.Component {
   render(){
     return (
       <Router>
-        {/* <div>
-        
-        </div> */}
-        <Route path="/" component={Home}/>
+        <Navbar />
+        <Home />
+        {/* <Route path="/" component={Home}/> */}
+        <Route path="/login" exact component={Login}/>
         <Route path="/signup" exact component={Signup}/>
       </Router>
 
